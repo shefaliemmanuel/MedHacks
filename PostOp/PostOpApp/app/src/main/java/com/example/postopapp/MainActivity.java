13124be4305d3a9ebb8f263720316545f54f3776
Shaina Mae Mainar <mainarsa@cofc.edu>
2019-10-01 08:20:40 -0400

    Septec final project

--- PostOp/PostOpApp/app/src/main/java/com/example/postopapp/MainActivity.java
+++ PostOp/PostOpApp/app/src/main/java/com/example/postopapp/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
     //TODO: need to implement below.
     //String csvfileString = getPackageManager().getApplicationInfo().dataDir + File.separatorChar + "Normal.csv";
